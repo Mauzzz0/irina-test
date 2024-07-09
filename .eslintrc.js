b3507fd1fa7d8f833517fa88f7f46b40cd957b62
Ruslan Semak <ruslan.semack1501@gmail.com>
2024-07-09 07:53:07 +0300

    Добавлен линтер

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  root: true,
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/semi': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-eval': 'error',
+    semi: [1, 'always'],
+  },
+};
---
+++ src/main.ts
@@ -0,0 +1,27 @@
+const persons = [
+  { age: 10, name: 'Kostya' },
+  { age: 17, name: 'Kostya-2' },
+  { age: 30, name: 'Pavel' },
+  { age: 50, name: 'Pavel-2' },
+];
+// Функция про десятки
+// const adults = persons.filter((person) => person.age >= 18);
+// const olds = persons.filter((person) => person.age > 100);
+
+// console.log(olds);
+// []
+
+// console.log(adults);
+/* Вывод:
+[ { age: 30, name: 'Pavel' }, { age: 50, name: 'Pavel-2' } ]
+*/
+
+// console.log(persons);
+/* Вывод:
+[
+  { age: 10, name: 'Kostya' },
+  { age: 17, name: 'Kostya-2' },
+  { age: 30, name: 'Pavel' },
+  { age: 50, name: 'Pavel-2' }
+]
+*/
